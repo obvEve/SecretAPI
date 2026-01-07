90857d00b376cb29bab8503e00301b8ec3b1b8c7
Misfiy <85962933+obvEve@users.noreply.github.com>
2026-01-07 19:34:55 +0100

    TryGetTeleportLocation

--- SecretAPI/Extensions/RoomExtensions.cs
+++ SecretAPI/Extensions/RoomExtensions.cs
@@ -1,8 +1,11 @@
 ﻿namespace SecretAPI.Extensions
 {
     using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
     using LabApi.Features.Wrappers;
     using MapGeneration;
+    using PlayerRoles.FirstPersonControl;
+    using PlayerRoles.PlayableScps.Scp106;
     using UnityEngine;
 
     /// <summary>
@@ -10,6 +13,8 @@
     /// </summary>
     public static class RoomExtensions
     {
+        private const float DefaultLocateRadius = 10;
+
         private static readonly List<RoomName> KnownUnsafeRooms =
         [
             RoomName.HczTesla, // Instant death
@@ -35,7 +40,48 @@
             if (KnownUnsafeRooms.Contains(room.Name))
                 return false;
 
-            return Physics.Raycast(room.Position, Vector3.down, out _, 2);
+            return Physics.Raycast(room.Position, Vector3.down, out _, 2, FpcStateProcessor.Mask);
+        }
+
+        /// <summary>
+        /// Tries to get a location to teleport a <see cref="Player"/> to.
+        /// </summary>
+        /// <param name="player">The player to attempt to get a teleport position from.</param>
+        /// <param name="position">The position found if any, otherwise null.</param>
+        /// <param name="zone">If set to anything other than <see cref="FacilityZone.None"/> will only attempt to find in that zone.</param>
+        /// <param name="defaultRadius">The default radius allowed nea  the found spot.</param>
+        /// <returns>Whether a valid teleport position was correctly found.</returns>
+        public static bool TryGetTeleportLocation(this Player player, [NotNullWhen(true)] out Vector3? position, FacilityZone zone = FacilityZone.None, float defaultRadius = DefaultLocateRadius)
+        {
+            position = null;
+            return player.RoleBase is IFpcRole fpc && TryGetTeleportLocation(fpc, out position, zone);
+        }
+
+        /// <summary>
+        /// Tries to get a location to teleport a <see cref="IFpcRole"/> to.
+        /// </summary>
+        /// <param name="fpc">The <see cref="IFpcRole"/> to attempt to get a teleport position from.</param>
+        /// <param name="position">The position found if any, otherwise null.</param>
+        /// <param name="zone">If set to anything other than <see cref="FacilityZone.None"/> will only attempt to find in that zone.</param>
+        /// <param name="defaultRadius">The default radius allowed nea  the found spot.</param>
+        /// <returns>Whether a valid teleport position was correctly found.</returns>
+        public static bool TryGetTeleportLocation(this IFpcRole fpc, [NotNullWhen(true)] out Vector3? position, FacilityZone zone = FacilityZone.None, float defaultRadius = Scp106PocketExitFinder.RaycastRange)
+        {
+            position = null;
+
+            IEnumerable<Pose> poses = zone == FacilityZone.None
+                ? SafeLocationFinder.GetLocations(null, null)
+                : Scp106PocketExitFinder.GetPosesForZone(zone);
+
+            if (!poses.TryGetRandomValue(out Pose pose))
+                return false;
+
+            float radius = defaultRadius;
+            if (Room.TryGetRoomAtPosition(pose.position, out Room? room))
+                radius = Scp106PocketExitFinder.GetRaycastRange(room.Zone);
+
+            position = SafeLocationFinder.GetSafePosition(pose.position, pose.forward, radius, fpc.FpcModule.CharController);
+            return true;
         }
     }
 }
