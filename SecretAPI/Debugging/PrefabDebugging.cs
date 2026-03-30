cc77c27e1389aeaf732d4a22681dbe68dc36ecda
Eve <85962933+obvEve@users.noreply.github.com>
2026-03-30 19:41:45 +0200

    Prefab Debugging

---
+++ SecretAPI/Debugging/PrefabDebugging.cs
@@ -0,0 +1,51 @@
+﻿#if DEBUG
+
+namespace SecretAPI.Debugging;
+
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using LabApi.Events.Handlers;
+using Mirror;
+using SecretAPI.Attributes;
+using SecretAPI.Features;
+using UnityEngine;
+using Logger = LabApi.Features.Console.Logger;
+
+/// <summary>
+/// Debugs basegame prefabs by logging information about them.
+/// </summary>
+internal static class PrefabDebugging
+{
+    /// <summary>
+    /// Loads the prefab debugging.
+    /// </summary>
+    [CallOnLoad]
+    internal static void Load()
+    {
+        ServerEvents.WaitingForPlayers += OnWaiting;
+    }
+
+    private static void OnWaiting()
+    {
+        foreach (PropertyInfo properties in typeof(PrefabManager).GetProperties())
+        {
+            try
+            {
+                if (properties.GetValue(null) == null)
+                    Logger.Error($"[PrefabDebugging] {properties.Name} returned a null value!");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error($"[PrefabDebugging] {properties.Name} ran into an exception: {ex}");
+            }
+        }
+
+        foreach (KeyValuePair<uint, GameObject> pair in NetworkClient.prefabs)
+        {
+            Logger.Debug($"[PrefabDebugging] Key ({pair.Key}) - Value ({pair.Value.name})");
+        }
+    }
+}
+
+#endif
--- SecretAPI/Features/PrefabManager.cs
+++ SecretAPI/Features/PrefabManager.cs
@@ -2,8 +2,11 @@
 
 using System;
 using System.Linq;
+using AdminToys;
 using Interactables.Interobjects;
 using MapGeneration;
+using Mirror;
+using UnityEngine;
 
 /// <summary>
 /// Manages prefabs that have variants and cannot be easily used within <see cref="PrefabStore{TPrefab}"/>.
@@ -15,6 +18,12 @@ public static class PrefabManager
     private const string HczBulkDoorName = "HCZ BulkDoor";
     private const string EzDoorName = "EZ BreakableDoor";
 
+    private const string EzArmCameraToyName = "EzArmCameraToy";
+    private const string EzCameraToyName = "EzCameraToy";
+    private const string LczCameraToyName = "LczCameraToy";
+    private const string HczCameraToyName = "HczCameraToy";
+    private const string SzCameraToyName = "SzCameraToy";
+
     /// <summary>
     /// Gets the <see cref="ReferenceHub"/> prefab.
     /// </summary>
@@ -23,24 +32,49 @@ public static class PrefabManager
     /// <summary>
     /// Gets the <see cref="BasicDoor"/> found in <see cref="FacilityZone.LightContainment"/>.
     /// </summary>
-    public static BasicDoor LczDoorPrefab => field ??= GetDoor(LczDoorName);
+    public static BasicDoor LczDoorPrefab => field ??= GetOrThrow<BasicDoor>(LczDoorName);
 
     /// <summary>
     /// Gets the <see cref="BasicDoor"/> found in <see cref="FacilityZone.HeavyContainment"/>.
     /// </summary>
-    public static BasicDoor HczDoorPrefab => field ??= GetDoor(HczDoorName);
+    public static BasicDoor HczDoorPrefab => field ??= GetOrThrow<BasicDoor>(HczDoorName);
 
     /// <summary>
     /// Gets the <see cref="BasicDoor"/> found in <see cref="FacilityZone.HeavyContainment"/>.
     /// </summary>
-    public static BasicDoor HczBulkDoorPrefab => field ??= GetDoor(HczBulkDoorName);
+    public static BasicDoor HczBulkDoorPrefab => field ??= GetOrThrow<BasicDoor>(HczBulkDoorName);
 
     /// <summary>
     /// Gets the <see cref="BasicDoor"/> found in <see cref="FacilityZone.Entrance"/>.
     /// </summary>
-    public static BasicDoor EzDoorPrefab => field ??= GetDoor(EzDoorName);
+    public static BasicDoor EzDoorPrefab => field ??= GetOrThrow<BasicDoor>(EzDoorName);
 
-    private static BasicDoor GetDoor(string name)
-        => PrefabStore<BasicDoor>.AllComponentPrefabs.FirstOrDefault(d => d.name == name)
-           ?? throw new InvalidOperationException($"[PrefabManager] Failed to get door named {name} | Report this as a bug!");
+    /// <summary>
+    /// Gets the <see cref="Scp079CameraToy"/> found in <see cref="FacilityZone.Entrance"/>, with an arm extension.
+    /// </summary>
+    public static Scp079CameraToy EzArmCameraToyPrefab => field ??= GetOrThrow<Scp079CameraToy>(EzArmCameraToyName);
+
+    /// <summary>
+    /// Gets the <see cref="Scp079CameraToy"/> found in <see cref="FacilityZone.Entrance"/>.
+    /// </summary>
+    public static Scp079CameraToy EzCameraToyPrefab => field ??= GetOrThrow<Scp079CameraToy>(EzCameraToyName);
+
+    /// <summary>
+    /// Gets the <see cref="Scp079CameraToy"/> found in <see cref="FacilityZone.LightContainment"/>.
+    /// </summary>
+    public static Scp079CameraToy LczCameraToyPrefab => field ??= GetOrThrow<Scp079CameraToy>(LczCameraToyName);
+
+    /// <summary>
+    /// Gets the <see cref="Scp079CameraToy"/> found in <see cref="FacilityZone.HeavyContainment"/>.
+    /// </summary>
+    public static Scp079CameraToy HczCameraToyPrefab => field ??= GetOrThrow<Scp079CameraToy>(HczCameraToyName);
+
+    /// <summary>
+    /// Gets the <see cref="Scp079CameraToy"/> found in <see cref="FacilityZone.Surface"/>.
+    /// </summary>
+    public static Scp079CameraToy SzCameraToyPrefab => field ??= GetOrThrow<Scp079CameraToy>(SzCameraToyName);
+
+    private static T GetOrThrow<T>(string name)
+        where T : NetworkBehaviour => PrefabStore<T>.AllComponentPrefabs.FirstOrDefault(c => c.name == name)
+                                      ?? throw new InvalidOperationException($"[PrefabManager] Failed to component ({typeof(T).Name}) by name {name} | Report this as a bug");
 }
